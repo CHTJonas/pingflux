a79561bd498d4a9b81406a5df2e95b4f002defd6
Charlie Jonas <charlie@charliejonas.co.uk>
2021-02-07 23:01:44 +0000

    Set ICMP message size appropriately for minimum MTU

--- internal/hosts/endpoint.go
+++ internal/hosts/endpoint.go
@@ -1,6 +1,7 @@
 package hosts
 
 import (
+	"net"
 	"time"
 
 	"github.com/go-ping/ping"
@@ -23,8 +24,18 @@ func (e *Endpoint) Ping(count int) (*ping.Statistics, error) {
 	}
 	pinger.SetPrivileged(true)
 	pinger.Count = count
+	if e.isIPv4() {
+		pinger.Size = 40
+	} else {
+		pinger.Size = 1232
+	}
 	pinger.Interval = time.Second
 	pinger.Timeout = time.Second * 10
 	pinger.Run() // blocks until finished
 	return pinger.Statistics(), nil
+}
+
+func (e *Endpoint) isIPv4() bool {
+	ip := net.ParseIP(e.IP)
+	return len(ip.To4()) == net.IPv4len
 }
